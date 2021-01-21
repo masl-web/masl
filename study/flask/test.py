2746f1bef1e6495f5279924b00938b73fadb43f0
ksy <ksy9926@naver.com>
2021-01-22 02:24:13 +0900

    코드리뷰 받은거  수정

--- study/flask/test.py
+++ study/flask/test.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template, make_response
 from view import blog
 import requests
+
 app = Flask(__name__, static_url_path='/static')
 # Flask 객체를 app에 할당
 app.register_blueprint(blog.blog_abtest, url_prefix='/blog')
@@ -10,20 +11,33 @@ def student():
    return render_template('give.html')
 #라우팅을 통해 URL을 해당 URL에 맞는 기능과 연결해 줌
 
-@app.route('/result',methods = ['POST', 'GET'])
+@app.route('/result', methods=['POST', 'GET'])
 def result():
    if request.method == 'POST':
       result = request.form
-      if result['Area'] not in ['강남', '강북', '강동', '강서'] or result['Distance'] not in ['100m', '250m', '500m', '1km'] or result['Price'] not in ['~50만', '~60만', '~70만', '70만 이상']:
+      correct_area = ['강남', '강북', '강동', '강서']
+      correct_distance = ['100m', '250m', '500m', '1km']
+      correct_price = ['~50만', '~60만', '~70만', '70만 이상']
+
+      check_area = result['Area'] not in correct_area
+      check_distance = result['Distance'] not in correct_distance
+      check_price = result['Price'] not in correct_price
+
+      if check_area or check_distance or check_price:
         return render_template("contenterror.html")
       return render_template("result.html",result = result)
 
-@app.route('/result2',methods = ['POST', 'GET'])
+@app.route('/result2', methods=['POST', 'GET'])
 def result2():
    if request.method == 'POST':
       result2 = request.form
       return render_template("result2.html")
 
 if __name__=='__main__':
-    app.run(host = '127.0.0.1', port = '5001', debug=True)
-#웹 서버 구동. 직접 실행시만 실행. 안될시 포트번호 바꿔보기
+    app.run(debug=True)
+#linux환경에서 실행 시 아래 코드를 입력
+#5000포트 사용시 제 컴퓨터 환경에서는 실행이 안되서 우선 8080포트 사용했습니다.
+# export FLASK_APP=test.py
+# export FLASK_RUN_HOST=127.0.0.1
+# export FLASK_RUN_PORT=8080
+# flask run
